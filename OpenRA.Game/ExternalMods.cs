e046965864aea291ec1fc9a895fe04a513d297aa
Yaobin Wen <robin.wyb@gmail.com>
2024-02-17 12:47:32 +0000

    Display where the external mods are loaded

--- OpenRA.Game/ExternalMods.cs
+++ OpenRA.Game/ExternalMods.cs
@@ -77,6 +77,7 @@ namespace OpenRA
 				{
 					try
 					{
+						Console.WriteLine($"Loading external mod at {path}");
 						var yaml = MiniYaml.FromFile(path, stringPool: stringPool).First().Value;
 						LoadMod(yaml, path);
 					}
