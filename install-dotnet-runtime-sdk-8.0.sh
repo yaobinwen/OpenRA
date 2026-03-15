81c6bd0281ec82688e384ecbdc74fda519f777f3
Yaobin Wen <robin.wyb@gmail.com>
2026-03-15 18:56:25 -0400

    Latest OpenRA requires .NET runtime and SDK 8.0 to compile

--- install-dotnet-runtime-sdk-8.0.sh
+++ install-dotnet-runtime-sdk-8.0.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# https://learn.microsoft.com/en-us/dotnet/core/install/linux-ubuntu-1804
+# https://learn.microsoft.com/en-us/dotnet/core/install/linux-ubuntu-install?tabs=dotnet10&pivots=os-linux-ubuntu-2404#ubuntu-2404
 
 sudo -v
 
@@ -9,4 +9,4 @@ sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 
 sudo apt-get update
-sudo apt-get install -y dotnet-runtime-6.0
+sudo apt-get install -y dotnet-runtime-8.0 dotnet-sdk-8.0
